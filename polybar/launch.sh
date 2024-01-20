ef7b84ce5667c9a8ba06a38d2e1c87670a0de041
RaCl <rajclerk.2004@gmail.com>
2024-01-21 02:17:34 +0530

    [updated] added status bar applets, updated colors and misc.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,6 +4,7 @@ polybar-msg cmd quit
 
 echo "---" | tee -a /tmp/polybar.log
 
-polybar example 2>&1 | tee -a /tmp/polybar.log & disown
-
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload example &
+done
 echo "Bars launched"
